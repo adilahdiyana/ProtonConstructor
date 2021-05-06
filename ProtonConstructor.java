dd93b708a3ed8d3bb3e4963f641fc985d75960fc
adilahdiyana <81138230+adilahdiyana@users.noreply.github.com>
2021-05-06 13:48:30 +0800

    Create ProtonConstructor.java

---
+++ ProtonConstructor.java
@@ -0,0 +1,36 @@
+package Protonconstructor;
+
+import java.util.Scanner;
+
+public class Proton {
+	
+	String brand;
+	String color;
+	int maxSpeed;
+	
+	Scanner s = new Scanner(System.in);
+	//default constructor
+	//Proton p = new Proton();
+	Proton() {
+		
+		System.out.println("Enter Max Speed");
+		this.maxSpeed = s.nextInt();
+		s.nextLine();
+		System.out.println("Enter Brand");
+		this.brand = s.nextLine();
+		s.nextLine();		
+		System.out.println("Enter Color");
+		this.color = s.nextLine();
+		s.nextLine();
+			
+	}
+	
+	
+	//Parameterized constructor
+	Proton(String b, String c, int s){
+			this.brand=b;
+			this.color=c;
+			this.maxSpeed=s;
+	}
+
+}
